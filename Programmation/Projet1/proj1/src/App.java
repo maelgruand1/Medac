9e44f2ead3a71bc540898d283f29ef3df8889cc0
Maël Gruand <maelgruand1@yahoo.com>
2024-12-12 13:59:04 +0100

    modif

--- Programmation/Projet1/proj1/src/App.java
+++ Programmation/Projet1/proj1/src/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");    
+        User.main(args);
+    }
+}
+class User {
+    private static String pedirNombre(Scanner input, String nombre) {
+        nombre = input.nextLine();
+        return nombre;
+    }
+    private static int pedirEdad(Scanner input) {
+        int edad = input.nextInt();
+        return edad;
+    }
+    // verificar la edad
+    private static int verificarEdad(int age) {
+        if (age >= 18) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduzca su nombre: ");
+        String nombre = pedirNombre(scanner, "");
+        System.out.print("Introduzca su edad: ");
+        int edad = pedirEdad(scanner);
+        int resultado = verificarEdad(edad);
+        if (resultado == 1) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
     }
 }
